ffd9d88d134697d5934957ba3ebafc57dfd43d91
lukas <lukas.westermann@gmail.com>
2009-11-03 16:30:17 +0100

    added rdoc

--- lib/rfc2425/utils.rb
+++ lib/rfc2425/utils.rb
@@ -1,5 +1,12 @@
 module Rfc2425
   module Utils
+    
+    # Convert to valid and complete property name, normally
+    # the results are upcased automatically.
+    #
+    #   Rfc2425::Utils.namerize(:name) # => "NAME"
+    #   Rfc2425::Utils.namerize('x_aim') # => "X-AIM"
+    #
     def namerize(str, upcase = true)
       str = str.to_s.gsub(/_/, '-').gsub(/\s/, '')
       str.upcase! if upcase
@@ -7,6 +14,7 @@ module Rfc2425
     end
     module_function :namerize
     
+    # Escape that string, as described in RFC.
     def escape(str)
       str.to_s.gsub(/([,;:\\])/, '\\\\\1').gsub(/\r?\n/, '\n')
     end
